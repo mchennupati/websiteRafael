ac0fc062eed94c130a6d77d631a5c9de012519b7
nnechm <m.chennupati@gmail.com>
2021-05-22 09:14:40 +0200

    updated colour and added links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome to the Gaming Website</h1>
+      <h3> Good Games :</h3>
+      <a href="https://www.fortnite.com" target="_blank" rel="noreferrer">
+        {" "}
+        Fortnite{" "}
+      </a>{" "}
+      <br />
+      <a href="https://www.minecraft.net" target="_blank" rel="noreferrer">
+        {" "}
+        Minecraft{" "}
+      </a>
+      <a href="https://www.roblox.com" target="_blank" rel="noreferrer">
+        {" "}
+        Roblox{" "}
+      </a>
+      <a href="https://www.nintendo.com" target="_blank" rel="noreferrer">
+        {" "}
+        Nintendo{" "}
+      </a>
     </div>
   );
 }
